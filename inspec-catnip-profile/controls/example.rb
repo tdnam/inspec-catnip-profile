e37fb1b2237bede034eed51c2c85b550e8498094
Nam Tran <tdnam39@gmail.com>
2020-02-04 15:54:37 +1100

    test

--- inspec-catnip-profile/controls/example.rb
+++ inspec-catnip-profile/controls/example.rb
@@ -8,11 +8,11 @@ describe file("/tmp") do
 end
 
 # you add controls here
-control "tmp-1.0" do                        # A unique ID for this control
-  impact 0.7                                # The criticality, if this control fails.
-  title "Create /tmp directory"             # A human-readable title
-  desc "An optional description..."
-  describe file("/tmp") do                  # The actual test
-    it { should be_directory }
-  end
-end
+# control "tmp-1.0" do                        # A unique ID for this control
+#   impact 0.7                                # The criticality, if this control fails.
+#   title "Create /tmp directory"             # A human-readable title
+#   desc "An optional description..."
+#   describe file("/tmp") do                  # The actual test
+#     it { should be_directory }
+#   end
+# end
